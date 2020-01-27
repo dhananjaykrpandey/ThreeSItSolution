df811759c27a50fe190a4955359f3fd110472ad5
Dhananjay Kr Pandey <dhananjaykrpandey@outlook.com>
2020-01-27 15:24:47 +0400

    Updated home page

--- ThreeSItSolution/Startup.cs
+++ ThreeSItSolution/Startup.cs
@@ -21,13 +21,14 @@ namespace ThreeSItSolution
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
